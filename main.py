43f67e6d62fa8d453755f7d4fde28be278176131
Stephan Kern <stephan@swmmrman.com>
2021-12-09 04:00:19 -0700

    Formatting

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ START_COLOR = "210000"
 KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'num0', 'num1', 'num2',
         'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9']
 
+
 subprocess.call(F"g910-led -a {BACKGROUND}", shell=True)
 cpu_count = psutil.cpu_count()
 init_string = "\\n"
@@ -15,8 +16,10 @@ init_string = F"echo -e '{init_string}c'"
 
 ret = subprocess.call(F"{init_string} | g910-led -pp", shell=True)  # ", shell=True)
 
+
 def get_color(percent):
     return round(percent/100) * 255
+
 
 while True:
     cpu = psutil.cpu_percent(interval=.1, percpu=True)
