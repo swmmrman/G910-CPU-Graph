46e2710909250028a231a25a0313c1c7e3805c4d
Stephan Kern <stephan@swmmrman.com>
2021-12-09 03:59:43 -0700

    Change Keys so 0-9 are the physical cores, and HT is on numpad 0-9

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import subprocess
 
 BACKGROUND = "562500"
 START_COLOR = "210000"
-KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '=', 'q', 'w',
-        'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f',
-        'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm',
-        ',', '.', '/']
+KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'num0', 'num1', 'num2',
+        'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9']
 
 subprocess.call(F"g910-led -a {BACKGROUND}", shell=True)
 cpu_count = psutil.cpu_count()
