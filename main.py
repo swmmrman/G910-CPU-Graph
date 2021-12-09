c73f6207f9c86d23ad9c8f41a2fef7e991ae8caa
Stephan Kern <stephan@swmmrman.com>
2021-12-08 18:16:18 -0700

    define a list of keys, and get cpu percents per core

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import psutil
+
+KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '=', 'q', 'w',
+        'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f',
+        'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+        ',', '.', '/']
+
+while True:
+    cpu = psutil.cpu_percent(interval=.25, percpu=True)
