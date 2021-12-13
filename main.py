8890bb70f972003847a7f488123e55c8fa81a3a0
Stephan Kern <stephan@swmmrman.com>
2021-12-13 14:44:39 -0700

    Shorten lines.

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ signal.signal(signal.SIGINT, stop)
 RUNNING = True
 BACKGROUND = "562500"
 START_COLOR = "210000"
-KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'num0', 'num1', 'num2',
-        'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9']
+KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'num0', 'num1',
+        'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9']
 
 
 subprocess.call(F"g910-led -a {BACKGROUND}", shell=True)
@@ -33,7 +33,7 @@ for core in range(0, cpu_count):
     init_string += F"k {KEYS[core]} {START_COLOR}\\n"
 init_string = F"echo -e '{init_string}c'"
 
-ret = subprocess.call(F"{init_string} | g910-led -pp", shell=True)  # ", shell=True)
+ret = subprocess.call(F"{init_string} | g910-led -pp", shell=True)
 
 
 def get_color(percent):
